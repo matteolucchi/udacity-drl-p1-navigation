abbf67b5f753a90ace2bbcec6aac50b4779a409d
Matteo Lucchi <matteo.lucchi@joanneum.at>
2019-06-26 15:02:03 +0200

    feat: added hyperparameters to class constructor

--- dqn_agent.py
+++ dqn_agent.py
@@ -20,7 +20,9 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, action_size, seed, buffer_size = BUFFER_SIZE, \
+                    batch_size = BATCH_SIZE, gamma = GAMMA, tau = TAU, lr = LR, \
+                    update_every = UPDATE_EVERY):
         """Initialize an Agent object.
         
         Params
@@ -36,24 +38,24 @@ class Agent():
         # Q-Network
         self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
         self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=lr)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
-        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.memory = ReplayBuffer(action_size, buffer_size, batch_size, seed)
+        # Initialize time step (for updating every update_every steps)
         self.t_step = 0
-    
+
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
-        
-        # Learn every UPDATE_EVERY time steps.
-        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+
+        # Learn every update_every time steps.
+        self.t_step = (self.t_step + 1) % update_every
         if self.t_step == 0:
             # If enough samples are available in memory, get random subset and learn
-            if len(self.memory) > BATCH_SIZE:
+            if len(self.memory) > batch_size:
                 experiences = self.memory.sample()
-                self.learn(experiences, GAMMA)
+                self.learn(experiences, gamma)
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
